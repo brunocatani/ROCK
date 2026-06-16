65c737b01e300ebc78e1158d4dfe9b04efd40ad0
Bruno Catani <brunohcatani@gmail.com>
2026-06-16 16:53:48 -0300

    fix/config: reload replaced ROCK ini files

--- src/RockConfig.cpp
+++ src/RockConfig.cpp
@@ -2217,7 +2217,9 @@ namespace rock
             ROCK_LOG_DEBUG(Config, "Starting file watch on '{}'", _iniFilePath);
 
             _fileWatch = std::make_unique<filewatch::FileWatch<std::string>>(_iniFilePath, [this](const std::string&, const filewatch::Event changeType) {
-                if (changeType != filewatch::Event::modified) {
+                if (changeType != filewatch::Event::modified &&
+                    changeType != filewatch::Event::added &&
+                    changeType != filewatch::Event::renamed_new) {
                     return;
                 }
 
