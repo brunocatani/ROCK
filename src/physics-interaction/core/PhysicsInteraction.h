eb6b7bee153c3ccce16ab435a4381f3eb6f0b674
Bruno Catani <brunohcatani@gmail.com>
2026-06-16 19:12:23 -0300

    fix/weapon: remove held equip phantom handoff

--- src/physics-interaction/core/PhysicsInteraction.h
+++ src/physics-interaction/core/PhysicsInteraction.h
@@ -28,7 +28,6 @@
 #include "physics-interaction/native/PhysicsStepDriveCoordinator.h"
 #include "physics-interaction/stash/ShoulderStashDetector.h"
 #include "physics-interaction/weapon/EquippedWeaponDropPolicy.h"
-#include "physics-interaction/weapon/HeldWeaponEquipVisualHandoff.h"
 #include "physics-interaction/weapon/TwoHandedGrip.h"
 #include "physics-interaction/weapon/WeaponCollision.h"
 #include "physics-interaction/weapon/WeaponDebug.h"
@@ -298,7 +297,6 @@ namespace rock
         BodyBoneColliderSet _bodyBoneColliders;
 
         WeaponCollision _weaponCollision;
-        HeldWeaponEquipVisualHandoff _heldWeaponEquipVisualHandoff;
 
         PhysicsStepDriveCoordinator _generatedBodyStepDrive;
 
