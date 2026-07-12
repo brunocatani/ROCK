8a801998e078ca7550786c409cd373e780d0c46d
Bruno Catani <brunohcatani@gmail.com>
2026-07-12 19:49:58 -0300

    build/grab: regenerate finger calibration header for hFRIK flex+splay inversion
    Hash-line refresh only (generator fingerprints hFRIK HandPose.cpp, which
    changed in hFRIK 8a1ff4d: mirrored left fingers now replay recovered
    flex [-1,2] + proximal splay through the left arcs).

--- src/physics-interaction/grab/GeneratedGrabFingerCalibration.h
+++ src/physics-interaction/grab/GeneratedGrabFingerCalibration.h
@@ -6,7 +6,7 @@
 // - hFRIK/src/skeleton/HandPoseData.cpp
 // - original frik deps/F4VR-CommonFramework/src/common/Quaternion.h
 // - original frik deps/F4VR-CommonFramework/src/common/MatrixUtils.cpp
-// Source SHA-256: 962af7591131c70307c3caa8cee19080c181e349d2b63448642092001e7e8997
+// Source SHA-256: 7397d5bf255d612df76aa0cd0f7eed3506030b2c3322e4558573f41eaf92d9e2
 
 #include <array>
 #include <cstddef>
@@ -16,7 +16,7 @@ namespace rock::grab_finger_calibration_data
 {
     inline constexpr std::uint32_t kGrabFingerCalibrationGeneratorVersion = 3;
     inline constexpr std::size_t kGrabFingerCalibrationSampleCount = 201;
-    inline constexpr const char* kGrabFingerCalibrationSourceHash = "962af7591131c70307c3caa8cee19080c181e349d2b63448642092001e7e8997";
+    inline constexpr const char* kGrabFingerCalibrationSourceHash = "7397d5bf255d612df76aa0cd0f7eed3506030b2c3322e4558573f41eaf92d9e2";
 
     enum class BakedGrabFingerProbe : std::uint8_t
     {
