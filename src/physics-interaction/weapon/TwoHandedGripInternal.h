83320332efb4e127b91e0ae6c7f8ffbfb0ad153f
Bruno Catani <brunohcatani@gmail.com>
2026-08-25 10:37:08 -0300

    cleanup/two-hand-debug: extract grip diagnostics

---
+++ src/physics-interaction/weapon/TwoHandedGripInternal.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "physics-interaction/weapon/GunstockAlignmentPolicy.h"
+#include "physics-interaction/weapon/TwoHandedGrip.h"
+
+namespace rock::two_handed_grip_internal
+{
+    inline constexpr std::uint32_t SCOPE_TRANSITION_TRACE_FRAMES = 6;
+
+    [[nodiscard]] bool isFiniteTransform(const RE::NiTransform& transform);
+    [[nodiscard]] bool tryGetRootFlattenedHandBoneTransform(
+        bool isLeft,
+        RE::NiTransform& outTransform);
+    [[nodiscard]] gunstock_alignment_policy::FineTuneDegrees configuredGunstockFineTune();
+}
